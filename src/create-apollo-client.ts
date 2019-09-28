1148fb67ea41b61348e8a32537bf365dfa3ec0a6
kitze <kristijan.mkd@gmail.com>
2019-09-28 18:52:58 +0200

    param for url

--- src/create-apollo-client.ts
+++ src/create-apollo-client.ts
@@ -1,7 +1,7 @@
 import ApolloClient from 'apollo-boost';
 import { requestWithAuthorization } from './request-with-authorization';
 
-export const createApolloClient = url =>
+export const createApolloClient = (url: string) =>
   new ApolloClient({
     uri: url,
     request: requestWithAuthorization,
